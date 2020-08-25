bda9ee3be9870da03de7e37a890e2cfc4ef2413b
Jesús Ramos Rodríguez <jeramauni@gmail.com>
2020-08-25 16:01:37 +0200

    Cards movement and basic feedback

---
+++ Slay the Porro/Assets/Scripts/ScaleFeedback.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleFeedback : MonoBehaviour
+{
+    public void OnMouseEnter()
+    {
+        transform.localScale += new Vector3(0.1f, 0.1f, 0);
+    }
+
+    public void OnMouseOver()
+    {
+    }
+
+    public void OnMouseExit()
+    {
+        transform.localScale -= new Vector3(0.1f, 0.1f, 0);
+    }
+}
---
+++ Slay the Porro/Assets/Scripts/Stats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Stats.Card_Stats
+{
+    static class Default
+    {
+        const bool targeteable = false;
+        const bool physical_dmg = false;
+        const bool water_dmg = false;
+        const bool fire_dmg = false;
+    }
+
+    static class JesusEnLasDrogas
+    {
+        const bool targeteable = true;
+        const bool physical_dmg = true;
+        const bool water_dmg = false;
+        const bool fire_dmg = false;
+    }
+}
